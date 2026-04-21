943d7d84e7c3507e339de0b6feaea93cb55bb6b6
João Vitor Coutinho <coutinhoasdf@gmail.com>
2026-04-20 23:55:45 -0300

    Alterando caminhos do site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '//simulador-rescisao/', 
+  base: '/CALCULADORA-RESCISAO/', // ATENÇÃO AQUI: Tem que ter a barra antes e depois!
 })
